6a0ceedd9544c86a7e9ef6fded99ccfe6aaa3ebb
Volodymyr Peschanenko <volodymyr.peschanenko@garuda.ai>
2022-03-23 18:24:03 +0200

    restore

--- midend/interpreter.cpp
+++ midend/interpreter.cpp
@@ -877,17 +877,19 @@ void ExpressionEvaluator::postorder(const IR::Operation_Unary* expression) {
         set(expression, result);
         return;
     }
-    if (auto cast = expression->to<IR::Cast>()) {
-        if (cast->destType->is<IR::Type_Boolean>() && l->is<SymbolicInteger>()) {
-            l = new SymbolicBool(sv->state);
-        } else if (cast->destType->is<IR::Type_Bits>() && l->is<SymbolicBool>()) {
-            l = new SymbolicInteger(sv->state, cast->destType->to<IR::Type_Bits>());
-        } else {
-            BUG_CHECK(!l->is<SymbolicInteger>() || !l->is<SymbolicBool>(),
-                        "%1% unexpected type %2% in cast", cast, cast->destType);
+    if (sv->isUnknown()) {
+        if (auto cast = expression->to<IR::Cast>()) {
+            if (cast->destType->is<IR::Type_Boolean>() && l->is<SymbolicInteger>()) {
+                l = new SymbolicBool(sv->state);
+            } else if (cast->destType->is<IR::Type_Bits>() && l->is<SymbolicBool>()) {
+                l = new SymbolicInteger(sv->state, cast->destType->to<IR::Type_Bits>());
+            } else {
+                BUG_CHECK(!l->is<SymbolicInteger>() || !l->is<SymbolicBool>(),
+                            "%1% unexpected type %2% in cast", cast, cast->destType);
+            }
+            set(expression, l);
+            return;
         }
-        set(expression, l);
-        return;
     }
 
     if (l->is<SymbolicInteger>()) {
